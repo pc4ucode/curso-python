2e2395d21b5a30299931b7754ea9261130e068ac
euler.amorim <euler.amorim@hotmail.com>
2026-05-26 16:54:23 -0300

    Add new Python lessons on while loops, for loops, and list operations; implement a guessing game and demonstrate list methods

--- python_beginner/aula40.py
+++ python_beginner/aula40.py
@@ -5,6 +5,8 @@ while True:
     operador = input('Digite o operador (+-/*): ')
 
     numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
 
     try:
         num_1_float = float(numero_1)
@@ -28,6 +30,16 @@ while True:
         continue
 
     ###
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float} = {num_1_float + num_2_float}')
+    elif operador == '-':
+        print(f'{num_1_float} - {num_2_float} = {num_1_float - num_2_float}')
+    elif operador == '/':
+        print(f'{num_1_float} / {num_2_float} = {num_1_float / num_2_float}')
+    elif operador == '*':
+        print(f'{num_1_float} * {num_2_float} = {num_1_float * num_2_float}')
+    else:
+        print('Nunca deveria chegar aqui.')
 
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
 
---
+++ python_beginner/aula44.py
@@ -0,0 +1,8 @@
+"""
+Aula 44 - For + Range
+range -> range(start, stop, step)
+"""
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
